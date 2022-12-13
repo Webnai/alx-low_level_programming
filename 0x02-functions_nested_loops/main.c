0acacfe1bbb999f150920a67b29b9eda15ac980c
Webnai <wilbertboadzo144@gmail.com>
2022-12-13 06:51:24 -0800

    _putchar

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- *
+ * 
  * Return: Always 0 (Success)
- *
  */
 int main(void)
 {
-	char c = '-';
-
-	putchar(c);
-
-	putchar('\n');
-
-	return 0;
+	return (0);
 }

7cba1770859712debace10c22c23274cd310cf23
Webnai <wilbertboadzo144@gmail.com>
2022-12-14 08:27:29 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit numbers
+ * new line follows
+ * Return: Always 0 success
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}

6333a34a1f260cbca485e7b2600a734a42f21513
Webnai <wilbertboadzo144@gmail.com>
2022-12-20 06:18:24 -0800

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string
+ *
+ * @str: pointer
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}

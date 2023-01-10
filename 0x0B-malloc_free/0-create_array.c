12d130deb2ccfe82414859c06c34ae0d8e67191d
Webnai <wilbertboadzo144@gmail.com>
2023-01-10 07:01:48 -0800

    create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - prints buffer in hexa
+ * @c; the address of memory to print
+ * @size: the size fo the memory to print
+ *
+ * Return: Nothing.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i;
+
+	while (1)
+	{
+		s = malloc(size * sizeof(char));
+		if (s == NULL)
+			return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		s[i] = c;
+	return (s);
+}

df5921b8fe1ed32d499d6ad7f36426796c0d84a0
Webnai <wilbertboadzo144@gmail.com>
2023-01-11 05:12:49 -0800

    create array

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,12 @@
-#define _MAIN_H_
-#ifndef _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: Wilbert Boadzo
+ * Desc: Header file containing prototypes for all functions
+ * used in the 0x0B-malloc_free directory.
+ */
 
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);

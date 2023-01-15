9a669625037ad2177ed9086045a9b737d0f5f322
Webnai <wilbertboadzo144@gmail.com>
2023-01-15 06:30:08 -0800

    malloc checked

--- 0x0C-more_malloc_free/0-main.c
+++ 0x0C-more_malloc_free/0-main.c
@@ -19,8 +19,8 @@ int main(void)
 	printf("%p\n", (void *)c);
 	i = malloc_checked(sizeof(int) * 402);
 	printf("%p\n", (void *)i);
-	f = malloc_checkd(sizeof(float) * 100000000);
-	printf("p\n", (void *)f);
+	f = malloc_checked(sizeof(float) * 100000000);
+	printf("%p\n", (void *)f);
 	d = malloc_checked(INT_MAX);
 	printf("%p\n", (void *)d);
 	free(c);

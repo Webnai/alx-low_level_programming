f81c400a55ab06ac296e7ec49f811308bb4e84f4
Webnai <wilbertboadzo144@gmail.com>
2023-01-19 07:35:49 -0800

    op functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdli.h>
+
+/**
+ * op_add - sum two integers
+ * @num1:number 1
+ * @num2:number 2
+ *
+ * Return: an integer
+ */
+int op_add(int num1, int num2)
+{
+	return (num1 + num2);
+}
+
+/**
+ * op_sub - defference between intergers
+ * @num1:number 1
+ * @num2:number 2
+ *
+ * Return: an integer
+ */
+int op_sub(int num1, int num2)
+{
+	return (num1 - num2);
+}
+
+/**
+ * op_mul - multiplication between integers
+ * @num1:number 1
+ * @num2:number 2
+ *
+ * Return: an integer
+ */
+int op_mul(int num1, int num2)
+{
+	return (num1 * num2);
+}
+/**
+ * op_div - divison between integers
+ * @num1:number 1
+ * @num2:number 2
+ *
+ * Return: an integer
+ */
+int op_div(int num1, int num2)
+{
+	if (num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (num1 / num2);
+}
+
+/**
+ * op_mod - the remainder betwwen integers
+ * @num1:number 1
+ * @num2:number 2
+ *
+ * Return: an integer
+ */
+int op_mod(int num1, int num2)
+{
+	if (num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (num1 % num2);
+}

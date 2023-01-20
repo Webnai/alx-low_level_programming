93f298d52fe586ca9f8996be3c9b33960596e08c
Webnai <wilbertboadzo144@gmail.com>
2023-01-20 05:45:38 -0800

    name change

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -19,6 +19,12 @@ void reverse_array(int *a, int n);
  */
 char *string_toupper(char *);
 char *cap_string(char *);
+/**
+ * leet - switches characters to 1337
+ * @s: string to switch
+ *
+ * Return: returns character
+ */
 char *leet(char *);
 char *rot13(char *);
 void print_number(int n);

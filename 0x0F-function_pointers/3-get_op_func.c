1e0608d9a60bd330b24393d921a5095954effb7c
Webnai <wilbertboadzo144@gmail.com>
2023-01-20 04:27:30 -0800

    functions correctons

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,11 +20,12 @@ int (*get_op_func(char *s))(int a, int b)
 	int i;
 
 	i = 0;
-	while (i < 5)
+	while (i < 10)
 	{
-		if (s[0] == ops[i].op[0])
-			return ((ops[i].f));
+		if (s[0] == ops->op[i])
+			break;
 		i++;
 	}
-	return (NULL);
+
+	return (ops[i / 2].f);
 }

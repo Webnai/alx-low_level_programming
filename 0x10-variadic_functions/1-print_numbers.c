107ed3d177764567741113e0bed67285f973e291
Webnai <wilbertboadzo144@gmail.com>
2023-01-20 05:55:54 -0800

    print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers
+ * @n: number of parameter
+ * @separator: separator between two numbers
+ * Return: 0 always
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ls;
+
+	va_start(ls, n);
+
+	if (separator == NULL)
+		separator = "";
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ls, int));
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ls);
+}
